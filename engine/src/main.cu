6a70ca803eaf6f902d15bb9ec102dfb6722b88da
Fedotov Ilia <ilfedotov.04@icloud.com>
2025-08-25 23:03:53 +0400

    hello world

---
+++ engine/src/main.cu
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <time.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+typedef unsigned int uint32;
+
+#define check_cuda_errors(val) check_cuda(val, #val, __FILE__, __LINE__)
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line)
+{
+	if (result) {
+		std::cerr << "CUDA error: " << static_cast<uint32>(result) << " at " <<
+			file << ":" << line << " '" << func << "' \n";
+		cudaDeviceReset();
+		exit(99);
+	}
+}
+
+__global__ void render(double *fb, int imgwidth, int imgheight)
+{
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+	
+	if ((i >= imgwidth) || (j >= imgheight)) return;
+
+	int pixidx = j*imgwidth*3 + i*3;
+	fb[pixidx + 0] = double(i)/imgwidth;
+	fb[pixidx + 1] = double(j)/imgheight;
+	fb[pixidx + 2] = 0.0;
+}
+
+int main()
+{
+	int IMAGE_WIDTH  = 1200,
+		IMAGE_HEIGHT = 600;
+
+	int tx = 8,
+		ty = 8;
+
+	std::cerr << "Rendering a " << IMAGE_WIDTH << 'x' << IMAGE_HEIGHT << " image ";
+	std::cerr << "in " << tx << 'x' << ty << " blocks.\n";
+
+	int num_pix = IMAGE_WIDTH * IMAGE_HEIGHT;
+	size_t fb_size = 3*num_pix*sizeof(double);
+
+	double *fb;
+	check_cuda_errors(cudaMallocManaged((void **)&fb, fb_size));
+
+	clock_t start,
+			stop;
+
+	start = clock();
+	dim3 blocks(IMAGE_WIDTH/tx + 1, IMAGE_HEIGHT/ty + 1);
+	dim3 threads(tx, ty);
+	render<<<blocks, threads>>>(fb, IMAGE_WIDTH, IMAGE_HEIGHT);
+	check_cuda_errors(cudaGetLastError());
+	check_cuda_errors(cudaDeviceSynchronize());
+	stop = clock();
+
+	double timer = ((double)(stop-start)) / CLOCKS_PER_SEC;
+	std::cerr << "took " << timer << " seconds.\n";
+
+	std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+	for (int j = 0; j < IMAGE_HEIGHT; ++j) {
+		for (int i = 0; i < IMAGE_WIDTH; ++i) {
+			size_t pixidx = j*3*IMAGE_WIDTH + i*3;
+			double r = fb[pixidx + 0];
+			double g = fb[pixidx + 1];
+			double b = fb[pixidx + 2];
+
+			int ir = int(255.999 * r);
+			int ig = int(255.999 * g);
+			int ib = int(255.999 * b);
+			std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+		}
+	}
+	check_cuda_errors(cudaFree(fb));
+}
