fce09bc4b3a330c28ee87cc66c351d7dfabaff85
Fedotov Ilia <ilfedotov.04@icloud.com>
2025-09-23 18:10:00 +0400

    diffuse material

--- engine/src/main.cu
+++ engine/src/main.cu
@@ -27,16 +27,35 @@ void check_cuda(cudaError_t result, const char *const func, const char *const fi
 	}
 }
 
-__device__ color ray_color(const ray& r, hittable** world)
+#define RANDVEC3 vec3(curand_uniform(local_rand_state),curand_uniform(local_rand_state),curand_uniform(local_rand_state))
+__device__ vec3 random_unit_vector(curandState* local_rand_state)
 {
-	hit_record rec;
-	if ((*world)->hit(r, 0.0, DBL_MAX, rec)) {
-		return 0.5 * (rec.normal + color(1, 1, 1));
+	for (;;) {
+		vec3 p = 2.0 * RANDVEC3 - vec3(1,1,1);
+		double lensq = p.length();
+		if (1e-160 < lensq && lensq <= 1) { return p / sqrt(lensq); }
 	}
+}
 
-	vec3 unit_direction = unitv(r.direction());
-	double a = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+__device__ color ray_color(const ray& r, hittable** world, curandState* local_rand_state)
+{
+	ray current_ray = r;
+	double attenuation = 1.0;
+	for (int i = 0; i < 50; ++i) {
+		hit_record rec;
+		if ((*world)->hit(current_ray, 0.001, DBL_MAX, rec)) {
+			vec3 target = rec.p + rec.normal + random_unit_vector(local_rand_state);
+			attenuation *= 0.5;
+			current_ray = ray(rec.p, target-rec.p);
+		}
+		else {
+			vec3 unit_direction = unitv(current_ray.direction());
+			double a = 0.5 * (unit_direction.y() + 1.0);
+			vec3 c = (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+			return attenuation * c;
+		}
+	}
+	return color(0.0,0.0,0.0);
 }
 
 __global__ void rnd_render_init(int imgwidth, int imgheight, curandState* rand_state)
@@ -63,12 +82,12 @@ __global__ void render(vec3* fb, hittable** world, camera** cam, curandState* ra
 		double u = (i + curand_uniform_double(&local_rand_state) - 0.5) / imgwidth;
 		double v = (j + curand_uniform_double(&local_rand_state) - 0.5) / imgheight;
 		ray r = (*cam)->get_ray(u,v);
-		pixel_color += ray_color(r, world);
+		pixel_color += ray_color(r, world, &local_rand_state);
 	}
 	fb[pixidx] = pixel_color / samples_per_pixel;
 }
 
-__global__ void create_world(hittable** d_list, hittable** d_world, camera** d_camera, int imgwidth, int imgheight)
+__global__ void create_world(hittable** d_list, hittable** d_world, camera** d_camera)
 {
 	if (threadIdx.x == 0 &&  blockIdx.x == 0) {
 		*(d_list)   = new sphere(vec3(0,0,-1), 0.5);
@@ -88,6 +107,10 @@ __global__ void free_world(hittable** d_list, hittable** d_world, camera** d_cam
 
 int main()
 {	
+	size_t new_stack_size = 2 * 1024;
+	check_cuda_errors(cudaDeviceSetLimit(cudaLimitStackSize, new_stack_size));
+	std::cerr << "Stack size limit: " << new_stack_size << " bytes" << "\n";
+
 	int IMAGE_WIDTH       = 1200,
 		IMAGE_HEIGHT      = 600,
 		SAMPLES_PER_PIXEL = 100;
@@ -111,7 +134,7 @@ int main()
 	camera** d_camera;
 	check_cuda_errors(cudaMalloc((void**)&d_camera, sizeof(camera*)));
 
-	create_world<<<1,1>>>(d_list, d_world, d_camera, IMAGE_WIDTH, IMAGE_HEIGHT);
+	create_world<<<1,1>>>(d_list, d_world, d_camera);
 	check_cuda_errors(cudaGetLastError());
 	check_cuda_errors(cudaDeviceSynchronize());
 
